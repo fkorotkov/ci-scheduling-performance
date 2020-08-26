7bc6d5f0cfe0753e490933309c88b6ce8c858964
Fedor Korotkov <fedor.korotkov@gmail.com>
2020-08-26 13:36:11 -0400

    Create benchmark.sh

---
+++ benchmark.sh
@@ -0,0 +1,15 @@
+START=$(date +%s)
+DURRATION=$((60 * 60 * 24))
+UPTIME=$(($(date +%s) - $START))
+
+while [[ $UPTIME < $DURRATION ]]; do
+  date %s > updates.txt
+  git commit -m "Update on $(date)"
+  git push -u origin master 
+  
+  echo -n "Time remaining: "
+  echo $(($DURRATION - $UPTIME))
+
+  sleep 60
+  UPTIME=$(($(date +%s) - $START))
+done
