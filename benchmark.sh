0712518f5d6ae7a6f6ef3e297e46640daf4c1177
Fedor Korotkov <fedor.korotkov@gmail.com>
2020-08-26 15:13:10 -0400

    while true

--- benchmark.sh
+++ benchmark.sh
@@ -1,18 +1,10 @@
-START=$(date +%s)
-DURRATION=$((60 * 60 * 24))
-UPTIME=$(($(date +%s) - $START))
-
 set -e
 
-while [[ $UPTIME < $DURRATION ]]; do
+while true; do
   date +%s >> updates.txt
   git add updates.txt
   git commit -m "Update on $(date)"
   git push -u origin master 
-  
-  echo -n "Time remaining: "
-  echo $(($DURRATION - $UPTIME))
 
   sleep 180
-  UPTIME=$(($(date +%s) - $START))
 done
